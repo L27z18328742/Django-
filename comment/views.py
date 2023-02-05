00ad1ef4e020dc50ef5bac8f8243ee72d1909250
zewei <2718328742@qq.com>
2023-02-05 19:16:02 +0800

    回复功能

--- comment/views.py
+++ comment/views.py
@@ -1,22 +1,23 @@
-#-*- coding : utf-8-*-
+# -*- coding : utf-8-*-
 # coding:unicode_escape
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.decorators.clickjacking import xframe_options_exempt
+from mptt.templatetags import mptt_tags
 
 from article.models import ArticlePost
 from .forms import CommentForm
 from .models import Comment
+from notifications.signals import notify
 
 
 # Create your views here.
 
 @xframe_options_exempt
 def post_comment(request, article_id, parent_comment_id=None):
-    print("article: ",article_id)
     article = get_object_or_404(ArticlePost, id=int(article_id))
     # 处理POST请求
-    print("article: ",article)
     if request.method == "POST":
         comment_form = CommentForm(request.POST)
 
@@ -33,10 +34,26 @@ def post_comment(request, article_id, parent_comment_id=None):
                 # 被回复人
                 new_comment.reply_to = parent_comment.user
                 new_comment.save()
+                if not parent_comment.user.is_superuser:
+                    notify.send(
+                            request.user,
+                            recipient=parent_comment.user,
+                            verb='回复了你',
+                            target=article,
+                            action_object=new_comment,
+                        )
                 return HttpResponse('200 OK')
 
             new_comment.save()
-            return redirect("/article/detail/"+str(article_id)+"/")
+            if not request.user.is_superuser:
+                notify.send(
+                        request.user,
+                        recipient=User.objects.filter(is_superuser=1),
+                        verb='回复了你',
+                        target=article,
+                        action_object=new_comment,
+                    )
+            return redirect("/article/detail/" + str(article_id) + "/")
         else:
             return HttpResponse("表单内容有误，请重新填写。")
         # 处理错误请求
